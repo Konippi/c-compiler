9bfa369a6a52dabac182ce6f4c641db1975704d8
Konippi <konippi-114@outlook.jp>
2024-08-17 22:09:55 +0900

    chore: fix typo

--- src/2_create_simple_language/2-1_language_to_compile_single_integer/sample.c
+++ src/2_create_simple_language/2-1_language_to_compile_single_integer/sample.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "Not crrect number of arguments\n");
+    fprintf(stderr, "Not correct number of arguments\n");
     return 1;
   }
 
