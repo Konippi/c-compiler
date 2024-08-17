8aa917ab22e3ef38e96a5b5b9cfac058cd01f64a
Konippi <konippi-114@outlook.jp>
2024-08-17 13:06:47 +0900

    chore: add detailed description

--- src/1_learn_assembly/1-2_with_function_calls/sample.s
+++ src/1_learn_assembly/1-2_with_function_calls/sample.s
@@ -4,10 +4,10 @@
 plus:
     add rsi, rdi # first argument is stored in RDI, second argument is stored in RSI
     mov rax, rsi # return value is stored in RAX
-    ret          # return from plus function
+    ret          # return from plus function (pop the address from the stack, jump to the address)
 
 main:
     mov rdi, 1   # `1` is stored in RDI as the first argument
     mov rsi, 2   # `2` is stored in RSI as the second argument
-    call plus    # call plus function
+    call plus    # call plus function (push the next instruction address to the stack, jump to the plus function address)
     ret          # return from main function
